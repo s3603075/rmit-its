1600fa288077ab296df72e9e8eb7e027d6d13630
rmit-s3603075-danny-ho <s3603075@student.rmit.edu.au>
2017-08-20 15:19:11 +1000

    user ticket view

---
+++ public/js/tickets.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+    $("#email").keydown(function(e){
+        if(event.which == 13)   {
+            $("#btn-find").trigger('click');
+        }
+    });
+
+    $(".table-responsive").hide();
+    $("#not-found").hide();
+
+    $("#btn-find").click(function (e) {
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        })
+
+        e.preventDefault();
+
+        var formData = {
+            email: $('#email').val()
+        }
+
+        console.log(formData);
+
+        $.ajax({
+            type:"POST",
+            url: "search",
+            data: formData,
+            dataType:'json',
+            success: function(tickets)  {
+                console.log(tickets);
+                if(tickets.length != 0) {
+                    $(".table-responsive").show();
+                    $("#not-found").hide();
+                    $('#ticketlist').empty();
+                    $.each(tickets, function(i, ticket)  {
+                        var ticketView = '<tr><td>' + ticket.id + '</td>';
+                        ticketView += '<td>' + ticket.email + '</td>';
+                        ticketView += '<td>' + ticket.firstname + '</td>';
+                        ticketView += '<td>' + ticket.lastname + '</td>';
+                        ticketView += '<td>' + ticket.os + '</td>';
+                        ticketView += '<td><div class="text-center"><a href="/userticket/' + ticket.id + '" class="btn btn-default">View Details</a></div>'
+
+                        $("#ticketlist").append(ticketView);
+                    });
+                } else  {
+                    $(".table-responsive").hide();
+                    $("#not-found").show();
+                }
+
+            },
+            error: function()   {
+                console.log(error.stack());
+            }
+        });
+    });
+});
--- resources/views/pages/findticket.blade.php
+++ resources/views/pages/findticket.blade.php
@@ -1,73 +1,47 @@
 @extends('layouts.default')
 @section('content')
     <div class="container top-spacing">
-        <div class="col-md-8 col-md-offset-2">
-            <div>
-                <h2>Enter your email to check the status of your tickets</h2>
-            </div>
-            <div class="form-group">
-                {!! Form::label('email', 'Email Address') !!}
-                {!! Form::email('email', null, ['class' => 'form-control']) !!}
-            </div>
-            <button type="button" id="btn-find" name="btn-find" class="btn btn-primary">Submit</button>
+        <h1>Search for your tickets</h1>
+        <button type="button" class="btn btn-primary btn-lg bottom-spacing" data-toggle="modal" data-target="#myModal">Search Tickets</button>
+        <div class="table-responsive">
+            <table class="table">
+                <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Email</th>
+                    <th>Firstname</th>
+                    <th>Lastname</th>
+                    <th>OS</th>
+                    <th></th>
+                </tr>
+                </thead>
+                <tbody id="ticketlist">
+                </tbody>
+            </table>
         </div>
-        <div id="tickets" class="col-md-8 col-md-offset-2" style="padding-top:20px;">
+        <h2 id="not-found">
+            No results found.
+        </h2>
+    </div>
+    <div id="myModal" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Enter your email to check the status of your tickets</h4>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        {!! Form::label('email', 'Email Address') !!}
+                        {!! Form::email('email', null, ['class' => 'form-control']) !!}
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" id="btn-find" name="btn-find" class="btn btn-success" data-dismiss="modal">Submit</button>
+                    <button type="button" id="modal-close" class="btn btn-danger" data-dismiss="modal">Close</button>
+                </div>
+            </div>
         </div>
     </div>
-    <script>
-        $(document).ready(function() {
-            $("#email").keydown(function(e){
-               if(event.which == 13)   {
-                   $("#btn-find").trigger('click');
-               }
-            });
-
-            $("#btn-find").click(function (e) {
-                $.ajaxSetup({
-                    headers: {
-                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                    }
-                })
-
-                e.preventDefault();
-
-                var formData = {
-                    email: $('#email').val()
-                }
-
-                console.log(formData);
-
-                $.ajax({
-                   type:"POST",
-                    url: "search",
-                    data: formData,
-                    dataType:'json',
-                    success: function(tickets)  {
-                       console.log(tickets);
-                       $('#tickets').empty();
-                       if(tickets.length === 0) {
-                           $('#tickets').addClass("text-center");
-                           $("#tickets").append('<h2>No results found</h2>');
-                       }else    {
-                           $('#tickets').removeClass("text-center");
-                           $.each(tickets, function(i, ticket)  {
-                               var ticketView = '<div class="panel panel-primary"> <div class="panel-heading">Ticket #'+ ticket.id +'</div>';
-                               ticketView += '<div class="panel-body"> <div>'+ticket.issue +'</div></div>';
-                               ticketView += '<div class="panel-footer"> <div class="container-fluid">';
-                               ticketView += '<div class="col-sm-3 text-center"><a href="/userticket/'+ ticket.id +'" class="btn btn-default btn-lg">';
-                               ticketView += 'More info</a></div>';
-                               ticketView += '<div class="col-sm-3 col-sm-offset-6"> <div class="text-center"> <h4>'+ ticket.status + '</h4></div>';
-                               ticketView += '</div></div></div>';
-
-                               $("#tickets").append(ticketView);
-                           });
-                       }
-                    },
-                    error: function()   {
-                       console.log("error");
-                    }
-                });
-            });
-        });
-    </script>
+    {!! Html::script('js/tickets.js') !!}
 @stop
